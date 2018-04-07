6a48d73715609a22ad39d671b6d90e2866ac4926
Mitchell Ramsey <mitchellramsey1992@gmail.com>
2018-04-07 19:16:12 -0400

    storing changes

--- routes/userInfo-api-routes.js
+++ routes/userInfo-api-routes.js
@@ -1,29 +1,28 @@
 var db = require("../models");
-var userInfo = require("../models").UserInfo;
+// var userInfo = require("../models").UserInfo;
 module.exports = function(app) {
   app.post("/new", function (req, res) {
     console.log(req);
-    userInfo.create({
+    db.UserInfo.create({
       name: req.body.name,
       user_name: req.body.username,
       email: req.body.email,
-      password: req.body.psw
+      password: req.body.password
     })
-    // res.redirect("/homepage");
-  })
-};
+    res.redirect("/homepage");
+  });
 
-// Exporting the function
-module.exports = function(app) {
   app.get("/user", function(req,res) {
-      // Retrieving all database records
-      db.UserInfo.findAll({}).then(function(dbUserInfo) {
-          // Passing handlebars the data from findAll
-          var userObj = {
-              usersList: dbUserInfo
-          };
-          // Rendering courses and passing the data to be parsed on the handlebars page
-          res.json(userObj);
-      });
+    // Retrieving all database records
+    db.UserInfo.findAll({}).then(function(dbUserInfo) {
+        // Passing handlebars the data from findAll
+        var userObj = {
+            usersList: dbUserInfo
+        };
+        // Rendering courses and passing the data to be parsed on the handlebars page
+        res.json(userObj);
+    });
   });
 };
+
+
