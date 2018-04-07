498bc193ef896d1a1a451435ddae3863d13947f8
mramsey1992 <mitchellramsey1992@gmail.com>
2018-04-07 16:59:54 -0400

    saving to pull

--- routes/userInfo-api-routes.js
+++ routes/userInfo-api-routes.js
@@ -3,25 +3,24 @@ var db = require("../models");
 module.exports = function(app) {
   app.post("new/", function (req, res) {
     db.UserInfo.create({
-      user_name: req.body.name,
+      name: req.body.name,
+      user_name: req.body.username,
       email: req.body.email,
       password: req.body.psw
     })
     res.redirect("/homepage");
-  })
-};
+  });
 
-// Exporting the function
-module.exports = function(app) {
   app.get("/user", function(req,res) {
-      // Retrieving all database records
-      db.UserInfo.findAll({}).then(function(dbUserInfo) {
-          // Passing handlebars the data from findAll
-          var userObj = {
-              usersList: dbUserInfo
-          };
-          // Rendering courses and passing the data to be parsed on the handlebars page
-          res.render("user_main", userObj);
-      });
+    // Retrieving all database records
+    db.UserInfo.findAll({}).then(function(dbUserInfo) {
+        // Passing handlebars the data from findAll
+        var userObj = {
+            usersList: dbUserInfo
+        };
+        // Rendering courses and passing the data to be parsed on the handlebars page
+        res.render("user_main", userObj);
+    });
   });
 };
+
