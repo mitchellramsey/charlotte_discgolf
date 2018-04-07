7910205fd518cfd13e83fe5aaee0ba8e022b94bf
rpiccoline1 <rpiccoline@hotmail.com>
2018-04-07 10:44:35 -0400

    working basic auth

---
+++ routes/signin.js
@@ -0,0 +1,25 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+app.post("/api/models/user_info/", function(req, res) {
+  console.log(req.body)
+db.UserInfo.find({
+  where: {
+    email: req.body.email,
+    password: req.body.password,
+  }
+  
+})
+  .then(function (result) {
+    console.log(result);
+    if(result){
+      console.log("Access Granted!!")
+      res.redirect("/");
+    } else {
+      console.log("Username or Password was incorrect.")
+    }
+  });
+})
+}
+
