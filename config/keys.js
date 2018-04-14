8d478b011011e4104c3b24fc9b795967df5ec709
Anthony Salemo <31021304+Ajsalemo@users.noreply.github.com>
2018-04-14 08:34:45 -0400

    test

--- config/keys.js
+++ config/keys.js
@@ -5,13 +5,13 @@ module.exports = {
     google: {
         clientID: "857178444903-q7jqe9rfer8j7llgcq9mbfj8si5odnhu.apps.googleusercontent.com",
         clientSecret: "EpeeMxD2rfCguqAC4w1UTo2E",
-        callbackURL: "/auth/google/redirect"
+        callbackURL: "https://stormy-scrubland-88373.heroku.com/auth/google/redirect"
     },
 
     github: {
         clientID: "39a0d19c4fd3d6b28793",
         clientSecret: "90c0830525debfa5ddf2e3bd2d55f1d9b73b3879",
-        callbackURL: "/auth/github/callback"
+        callbackURL: "https://stormy-scrubland-88373.heroku.com/auth/github/callback"
     },
     // Cookie keys
     session: {
